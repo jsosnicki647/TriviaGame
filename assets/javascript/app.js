be37e9c40ba21ee6890e2d3d6369f3709ef9305a
Jesse Sosnicki <jesse.sosnicki@gmail.com>
2019-07-20 00:00:19 -0400

    updated CSS, added timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,43 +1,49 @@
-var timeRemaining = 30
-var intervalID
+var game = {
+
+    timeRemaining: 30,
+    intervalID: 0,
+    start: function(){
+        $("#timer").html("<h2>Time Remaining: " + game.timeRemaining + "</h2>")
+        $("#start").css("display","none")
+        $("#title").css("margin-bottom","5%")
+        $("#game-window").css("height","65%")
+        $("#game-window").css("background","#6C757D")
+        game.intervalID = setInterval(game.count, 1000)
+    },
+    count: function(){
+        game.timeRemaining--
+        var converted = game.timeConverter(game.timeRemaining)
+        $("#timer").html("<h2>Time Remaining: " + game.timeRemaining + "</h2>")
+        if(game.timeRemaining == 0){
+            clearInterval(game.intervalID)
+        }
+    },
+    timeConverter: function(t){
+        var minutes = Math.floor(t / 60)
+        var seconds = t - (minutes * 60)
+
+        if (seconds < 10) {
+            seconds = "0" + seconds
+        }
+
+        if (minutes === 0) {
+            minutes = "00"
+        }
+        else if (minutes < 10) {
+            minutes = "0" + minutes
+        }
+        return minutes + ":" + seconds
+    }
+}
+
+
 
 window.onload = function(){
-    $("#start").on("click", start)
-}
-
-function start(){
-    $("#timer").text(timeRemaining)
-    $("#start").css("display","none")
-    $("#title").css("margin-bottom","5%")
+    $("#start").on("click", game.start)
 }
 
 
-function count() {
 
-  // DONE: increment time by 1, remember we cant use "this" here.
-  timeRemaining--;
 
-  // DONE: Get the current time, pass that into the timeConverter function,
-  //       and save the result in a variable.
-  var converted = timeConverter(timeRemaining);
-  console.log(converted);
-}
 
-function timeConverter(t) {
 
-  var minutes = Math.floor(t / 60)
-  var seconds = t - (minutes * 60)
-
-  if (seconds < 10) {
-    seconds = "0" + seconds
-  }
-
-  if (minutes === 0) {
-    minutes = "00"
-  }
-  else if (minutes < 10) {
-    minutes = "0" + minutes
-  }
-
-  return minutes + ":" + seconds
-}
